64a518869924a8531730473446189c034d0082a0
BelKed <66956532+BelKed@users.noreply.github.com>
2021-04-02 12:53:08 +0200

    Format script.js

--- script/script.js
+++ script/script.js
@@ -1,33 +1,45 @@
 function navbar() {
-    var x = document.getElementById("link");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+	var x = document.getElementById("link");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
 }
 
 $(function () {
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top)
-                }, 800, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top)
+				}, 800, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
 
-    var url = window.location.pathname;
-    var urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
+	var url = window.location.pathname;
+	var urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
 
-    $('.navbar a').each(function () {
-        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-            $(this).addClass('active');
-            $(this).removeAttr("href");
-        }
-    });
+	$('.navbar a').each(function () {
+		if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+			$(this).addClass('active');
+			$(this).removeAttr("href");
+		}
+	});
+
+	// $.getJSON('https://api.github.com/repos/oslfrobot/oslfrobot.github.io/commits', function (data) {
+	// 	const hash = data[0].sha.slice(0, 8);
+
+	// 	$('head link').each(function () {
+	// 		this.href += "?v=" + hash;
+	// 	});
+
+	// 	$('head script').each(function () {
+	// 		this.src += "?v=" + hash;
+	// 	});
+	// });
 });
