b285e8350280a9d7cfbdf74a4e2b6a677b93a468
BelKed <66956532+BelKed@users.noreply.github.com>
2021-04-05 07:49:35 +0200

    Remove unneeded `<br>` tags

--- script/script.js
+++ script/script.js
@@ -8,13 +8,10 @@ function navbar() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	const height = document.getElementsByClassName("navbar")[0].clientHeight;
-
 	new SmoothScroll('a[href*="#"]:not([href="#"])', {
 		// easeInOutExpo
 		speed: 800,
 		speedAsDuration: true,
-		offset: height < 60 ? height * -1 : (height - 60) * -1,
 		customEasing: function (time) {
 			if (time == 0) return 0;
 			if (time == 1) return 1;
